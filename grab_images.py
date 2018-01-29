052734711a13e07fdda9fe1f4432f01ff75217ca
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-01-28 16:25:15 -0800

    Cleaning up

--- grab_images.py
+++ grab_images.py
@@ -2,31 +2,30 @@ import cv2
 import os
 import time
 
+# This script is based on a supporting file from Udacity Robotics Nanodegree (Term 2)
+
 # Run this script from the same directory as your Data folder
 
 # Grab your webcam on local machine
 cap = cv2.VideoCapture(0)
 
-# Give image a name type
-name_type = 'Small_cat'
-
-# Initialize photo count
-number = 0
+# Initialize photo count per class
+nums = dict()
 
 # Specify the name of the directory that has been premade and be sure that it's the name of your class
 # Remember this directory name serves as your datas label for that particular class
-data_dir = 'Data'
+data_dir = 'data'
 data_dir_stamp = time.strftime("%G%m%d_%H%M%S")
-# print("data_dir = {}".format(data_dir))
-set_dir = 'Cat'
 
 print ("Photo capture enabled! Press any key to take photos! Pressed key - is a class.")
 
-nums = dict()
-
+# Latest pressed key (e.g. class)
 curr_key = None
 
 def crop_resize_rect(frame, size):
+  """
+  Resize and crop a square image from the original frame.
+  """
   fr = frame
   wy, wx = frame.shape[0], frame.shape[1]
 
@@ -44,7 +43,9 @@ def crop_resize_rect(frame, size):
   fr = cv2.resize(fr,(size, size), interpolation = cv2.INTER_NEAREST)
   return fr
 
+# Loop until 'esc' key is pressed
 while True:
+
     # Read in single frame from webcam
     ret, frame = cap.read()
 
@@ -59,6 +60,7 @@ while True:
     # Use this line locally to display the current frame
     cv2.imshow('Color Picture', frame)
 
+    # Capture rate is 5 Hz
     key = cv2.waitKey(200)
 
     # Use esc to take photos when you're ready
@@ -70,8 +72,6 @@ while True:
 
         # If you want to resize the image
         # gray_resize = cv2.resize(gray,(360,360), interpolation = cv2.INTER_NEAREST)
-
-
 
     # Print key for debug
     if key > 0:
@@ -98,14 +98,12 @@ while True:
           nums[curr_key] = 0
 
         # Save the image
-        # cv2.imwrite('Data/' + set_dir + '/' + name_type + "_" + str(number) + ".png", frame)
         fname = os.path.join(dir_path, data_dir_stamp + '_' + str(nums[curr_key]) + ".png")
         print('fname = {}'.format(fname))
         cv2.imwrite(fname, frame)
 
         print ("Saving image {} : {} ".format(curr_key, str(nums[curr_key])))
         # print("nums = {}".format(nums))
-
 
 cap.release()
 cv2.destroyAllWindows()
